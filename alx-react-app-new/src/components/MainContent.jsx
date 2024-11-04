c95fc470248a837ec7ad1de2933fad2cca60afb0
Macray Billy <macraybilly66@gmail.com>
2024-11-04 23:07:03 +0300

    0. Apply Inline CSS Styling to React Components

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+    return (
+        <main style={{ padding: '20px', margin: '15px', backgroundColor: '#e0f7fa', borderRadius: '8px' }}>
+            <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;
