d652bf8cbbc675ba0938461e08001cbd4d2d21d3
Macray Billy <macraybilly66@gmail.com>
2024-11-06 02:47:24 +0300

    Updated Navbar

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={{ padding: '10px', backgroundColor: '#333', color: 'white' }}>
+    <nav style={{ padding: '10px', backgroundColor: '#333', color: 'white',display:"flex",justifyContent:"space-between" }}>
       <Link to="/" style={{ color: 'white', margin: '0 10px' }}>Home</Link>
       <Link to="/about" style={{ color: 'white', margin: '0 10px' }}>About</Link>
       <Link to="/services" style={{ color: 'white', margin: '0 10px' }}>Services</Link>
