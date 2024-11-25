a8f502f9750c8f22737e6c53e6c8dffb5a4dab95
Macray Billy <macraybilly66@gmail.com>
2024-11-25 15:12:04 +0300

    update

--- react-todo/src/components/__tests__/TodoList.test.js
+++ react-todo/src/components/__tests__/TodoList.test.js
@@ -1,5 +1,5 @@
 import { render, screen, fireEvent } from "@testing-library/react";
-import TodoList from "../components/TodoList";
+import TodoList from "../TodoList";
 
 // Test to check if the TodoList component renders correctly
 test("renders TodoList component", () => {
